ce753dcdd78cba1ef160811cc4e13115a7fa56bc
LessNick <breeze.fbn@gmail.com>
2019-04-25 23:29:52 +0300

    added bat files for mount, compile & run

--- mount.bat
+++ mount.bat
@@ -1,4 +1,6 @@
-echo select vdisk file="c:\Emu\pentevo\Unreal.dev\100.vhd" >tmp.sce
-echo attach vdisk >>tmp.sce
+echo select vdisk file="c:\Emu\pentevo\Unreal.dev\100.vhd" > tmp.sce
+echo attach vdisk >> tmp.sce
+echo select partition 1 >> tmp.sce
+echo assign letter=Z >> tmp.sce
 diskpart /s tmp.sce
 del tmp.sce
